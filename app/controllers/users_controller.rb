df45141f1746e65c940d47fc167f411027e15303
dvik1950 <despic.viktor@gmail.com>
2018-08-10 21:13:18 +0200

    Add micropost form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,13 +51,7 @@ class UsersController < ApplicationController
   end
 
 
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
+
 
   def correct_user
     @user = User.find(params[:id])
