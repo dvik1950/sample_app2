871e96aa60d29d518f62707f3ce878737c85d822
dvik1950 <despic.viktor@gmail.com>
2018-10-02 14:52:11 +0200

    Add follow w JS

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -52,6 +52,22 @@ class UsersController < ApplicationController
 
 
 
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+
 
   def correct_user
     @user = User.find(params[:id])
